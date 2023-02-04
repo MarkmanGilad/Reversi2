b8a70ad5d3dbda251a68a89af25ca01384cd7df2
MarkmanGilad <gilad@markman.co.il>
2023-02-04 19:56:50 +0200

    alpha beta

--- Game.py
+++ Game.py
@@ -4,6 +4,7 @@ from Reversi import Reversi
 from Human_Agent import Human_Agent
 from MinMaxAgent import MinMaxAgent
 from MinMaxAgent2 import MinMaxAgent2
+from AlphBetaAgent import AlphaBetaAgent
 
 import time
 
@@ -13,16 +14,17 @@ win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Reversi')
 environment = Reversi()
 graphics = Graphics(win, board = environment.state.board)
-player1 = Human_Agent(player=1)
-player2 = Human_Agent(player=2)
-# player1 = minMaxAgent(player = 1,depth = 3, environment=environment)
-# player1 = AlphaBetaAgent(player = 1,depth = 3)
+# player1 = Human_Agent(player=1)
+# player2 = Human_Agent(player=2)
+# player1 = MinMaxAgent(player = 1,depth = 3, environment=environment)
 # player2 = MinMaxAgent(player = 2,depth = 3, environment=environment)
-# player2 = AlphaBetaAgent(player = 2,depth = 4)
-player1 = MinMaxAgent2(player = 1,depth = 2, environment=environment)
+player1 = MinMaxAgent2(player = 1,depth = 3, environment=environment)
 player2 = MinMaxAgent2(player = 2,depth = 3, environment=environment)
+# player1 = AlphaBetaAgent(player = 1,depth = 3, environment=environment)
+# player2 = AlphaBetaAgent(player = 2,depth = 3, environment=environment)
 
 def main ():
+    start = time.time()
     run = True
     clock = pygame.time.Clock()
     graphics.draw()
@@ -54,6 +56,7 @@ def main ():
     score1, score2 = environment.state.score()
     print ("player 1: score = ", score1)
     print ("player 2: score = ", score2)
+    print (time.time() - start)
 
 
 def switchPlayers(player):
